e7010fae572e99e38164cdd92f964b2ffab1ead5
░▒▓█│【Walkman】│█▓▒░ <matthew.carter@structureit.net>
2020-03-24 13:52:24 +0200

    Moved page code to an actual controller method, and registered the controller

--- src/CraftCognitoAuth.php
+++ src/CraftCognitoAuth.php
@@ -12,13 +12,12 @@
 namespace structureit\craftcognitoauth;
 
 use structureit\craftcognitoauth\services\JWT as JWTService;
-use structureit\craftcognitoauth\services\CognitoJWK as CognitoJWKService;
 use structureit\craftcognitoauth\models\Settings;
 
 use Craft;
 use craft\base\Plugin;
-use craft\web\Application;
-
+use craft\events\RegisterUrlRulesEvent;
+use craft\web\UrlManager;
 use yii\base\Event;
 
 /**
@@ -60,69 +59,13 @@ class CraftCognitoAuth extends Plugin
         parent::init();
         self::$plugin = $this;
 
-        Craft::$app->on(Application::EVENT_INIT, function (Event $event) {
-            if (Craft::$app->request->fullPath === 'cognitoauth')
-            {
-                if (($jwt = self::$plugin->jWT->getJWTFromRequest()))
-                {
-                    // If the token passes verification
-                    if (($token = self::$plugin->jWT->parseAndVerifyJWT($jwt))) {
-                        // Look for the user
-                        $user = self::$plugin->jWT->getUserByJWT($token);
-
-                        // If we don't have a user, and we're NOT allowed to create one
-                        if (!$user && !self::$plugin->jWT->shouldAutoCreateUser()) {
-                            print('No craft user found, and autocreate disabled!');
-                            die();
-                        } elseif (!$user) { // If we don't have a user, but we ARE allowed to create one
-                            $user = self::$plugin->jWT->createUserByJWT($token);
-                        }
-
-                        // Attempt to login as the user we have found or created
-                        if (isset($user->id) && $user->id) {
-                            Craft::$app->user->loginByUserId($user->id);
-                            // redirect back to the homepage
-                            Craft::$app->getResponse()->redirect(UrlHelper::baseUrl())->send();
-                            die();
-                        } else {
-                            print('Unknown error getting/creating user!');
-                            die();
-                        }
-                    } else {
-                        print('Invalid token!');
-                        die();
-                    }
-                } else {
-                    // No jwt in QueryParams, try using javascript to extract from SearchParams
-                    print('
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <script type="text/javascript">
-            var params = window.location.hash.substr(1);
-            var searchParams = new URLSearchParams(params);
-            var id_token = searchParams.get("id_token");
-            if (id_token !== null) {
-                window.location.replace(window.location.pathname + "?jwt=" + id_token);
-                document.write("Redirecting...");
-            } else {
-                document.write("No Token Found!");
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['cognitologin']    = 'craft-cognito-auth/j-w-t/cognito-login';
             }
-        </script>
-    </head>
-    <body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
-        <noscript>
-            This page uses javascript to move the id_token to the query parameters. Either enable Javascript,
-            or manually edit the "#id_token=" part of the url to "?jwt=".
-        </noscript>
-    </body>
-</html>
-                    ');
-                    die();
-                }
-            }
-        });
+        );
 
         Craft::info(
             Craft::t(
--- src/controllers/JWTController.php
+++ src/controllers/JWTController.php
@@ -11,7 +11,10 @@
 
 namespace structureit\craftcognitoauth\controllers;
 
+use Craft;
+use craft\helpers\UrlHelper;
 use craft\web\Controller;
+use structureit\craftcognitoauth\CraftCognitoAuth;
 
 /**
  * @author    Mike Pierce
@@ -29,10 +32,78 @@ class JWTController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = [];
+    protected $allowAnonymous = [
+        'cognito-login',
+    ];
 
     // Public Methods
     // =========================================================================
+
+    public function actionCognitoLogin()
+    {
+        if (($jwt = CraftCognitoAuth::$plugin->jWT->getJWTFromRequest()))
+        {
+            // If the token passes verification
+            if (($token = CraftCognitoAuth::$plugin->jWT->parseAndVerifyJWT($jwt)))
+            {
+                // Look for the user
+                $user = CraftCognitoAuth::$plugin->jWT->getUserByJWT($token);
+
+                if (!$user && !CraftCognitoAuth::$plugin->jWT->shouldAutoCreateUser())
+                {   // If we don't have a user, and we're NOT allowed to create one
+                    return 'No craft user found, and autocreate disabled!';
+                }
+                elseif (!$user)
+                {   // If we don't have a user, but we ARE allowed to create one
+                    $user = CraftCognitoAuth::$plugin->jWT->createUserByJWT($token);
+                }
+
+
+                if (isset($user->id) && $user->id)
+                {   // Attempt to login as the user we have found or created
+                    Craft::$app->user->loginByUserId($user->id);
+                    // redirect back to the homepage
+                    return $this->redirect(UrlHelper::baseUrl());
+                }
+                else
+                {   // no user ID, something went wrong...
+                    return 'Unknown error getting/creating user!';
+                }
+            }
+            else
+            {   // parseToken or verifyToken failed
+                return 'Invalid token!';
+            }
+        }
+        else
+        {   // No jwt in QueryParams, try using javascript to extract from SearchParams
+            return '
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <script type="text/javascript">
+            var params = window.location.hash.substr(1);
+            var searchParams = new URLSearchParams(params);
+            var id_token = searchParams.get("id_token");
+            if (id_token !== null) {
+                window.location.replace(window.location.pathname + "?jwt=" + id_token);
+                document.write("Redirecting...");
+            } else {
+                document.write("No Token Found!");
+            }
+        </script>
+    </head>
+    <body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
+        <noscript>
+            This page uses javascript to move the id_token to the query parameters. Either enable Javascript,
+            or manually edit the "#id_token=" part of the url to "?jwt=".
+        </noscript>
+    </body>
+</html>
+            ';
+        }
+    }
 
     /**
      * @return mixed
